461a8b8b8f60426981c3adcf352f8ac2f01e24a4
Casey McNeil <caseymcneil@gmail.com>
2013-12-20 20:10:49 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeightablePractice::Application.config.secret_key_base = '6f14592a2f53c066befa8fe1a33ccdcf83f763561041f3e9c65f68edaaa856fd9efcf8266608ea5b57eceb4a6cddb5d7bb0db5d0727e81c06145bdef4dc62233'
